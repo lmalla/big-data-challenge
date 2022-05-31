f4bcc8d880e49e5b36b16cda55ceac4fa26fef0c
lmalla <53437791+lmalla@users.noreply.github.com>
2022-05-30 23:04:38 -0400

    Final Commit

---
+++ level-1/query.sql
@@ -0,0 +1,17 @@
+-- Camera database to check successful upload
+SELECT * FROM review_id_table;
+
+SELECT * FROM products;
+
+SELECT * FROM customers;
+
+SELECT * FROM vine_table;
+
+-- Watches database to check successful upload
+SELECT * FROM review_id_table_watch;
+
+SELECT * FROM products_watch;
+
+SELECT * FROM customers_watch;
+
+SELECT * FROM vine_table_watch;
